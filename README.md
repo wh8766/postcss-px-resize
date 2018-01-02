69137f961314e308d9a71a6fe9f70c3ba90cf9ed
W.Hao <wanghao29@lenovo.com>
2018-01-02 15:05:55 +0800

    add scale option

--- README.md
+++ README.md
@@ -4,4 +4,28 @@
 
 > A postcss plugin to resize px in src.
 
+Project reference：https://github.com/songsiqi/px2rem-postcss
+
 ## Use
+
+    npm install postcss-px-resize --save-dev
+
+In Webpack 3, add `.postcssrc.js` PostCSS config file:
+
+```js
+module.exports = () => ({
+    plugins: {
+        "autoprefixer":{
+            // remove: false
+            browsers: ['iOS >= 7', 'Android >= 4.1']
+        },
+        "postcss-px-resize": {
+            scale: 0.5
+        }
+    }
+})
+```
+
+## Options
+
+- scale: target scale value, default is 0.5, eg: 50px in scss file will resize to 25px.
--- test/test.js
+++ test/test.js
@@ -7,20 +7,10 @@ var pxResize = require('..');
 var postcss = require('postcss');
 var autoprefixer = require('autoprefixer');
 
-var opacity = function (css) {
-  css.walkDecls(function (decl) {
-    if (decl.prop === 'opacity') {
-      decl.parent.insertAfter(decl, {
-        prop: '-ms-filter',
-        value: '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + (parseFloat(decl.value) * 100) + ')"'
-      });
-    }
-  });
-};
 
 describe('postcss-px-resize', function () {
 
-  it('[default] should output right rem file', function () {
+  it('[default] should output right px resize file', function () {
     var srcPath = path.join(__dirname, 'source.css');
     var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
     var outputText = postcss().use(pxResize({scale: 0.5})).process(srcText).css;
@@ -28,15 +18,22 @@ describe('postcss-px-resize', function () {
     assert.equal(outputText, expectedText);
   });
 
-  // it('should get along well with other plugins', function () {
-  //   var srcPath = path.join(__dirname, 'source.css');
-  //   var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
-  //   var outputText = postcss()
-  //     .use(autoprefixer({browsers: ['iOS >= 6', 'Android >= 2.3']}))
-  //     .use(pxResize({remUnit: 75}))
-  //     .use(opacity)
-  //     .process(srcText).css;
-  //   var expectedText = fs.readFileSync(path.join(__dirname, 'dest.multiple.css'), {encoding: 'utf8'});
-  //   assert.equal(outputText, expectedText);
-  // });
+    it('[default] should output right px resize file without option', function () {
+        var srcPath = path.join(__dirname, 'source.css');
+        var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
+        var outputText = postcss().use(pxResize()).process(srcText).css;
+        var expectedText = fs.readFileSync(path.join(__dirname, 'dest.basic.css'), {encoding: 'utf8'});
+        assert.equal(outputText, expectedText);
+    });
+
+  it('should get along well with other plugins', function () {
+    var srcPath = path.join(__dirname, 'source.css');
+    var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
+    var outputText = postcss()
+      .use(autoprefixer({browsers: ['iOS >= 6', 'Android >= 2.3']}))
+      .use(pxResize({scale: 0.5}))
+      .process(srcText).css;
+    var expectedText = fs.readFileSync(path.join(__dirname, 'dest.multiple.css'), {encoding: 'utf8'});
+    assert.equal(outputText, expectedText);
+  });
 });
